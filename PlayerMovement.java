52b8d87d5d57c4c7966f410457b5f548c55eb722
tomagata <long.m.phm@gmail.com>
2024-10-17 10:10:31 +0200

    Update PlayerMovement.java

--- PlayerMovement.java
+++ PlayerMovement.java
@@ -1,11 +1,64 @@
-public class PlayerMovement {
+import java.awt.event.*;
 
-    int movement() {
-        return 0;
-    }
+public class PlayerMovement implements KeyListener {
     
-    public static void main(String[] args) {
-        (new PlayerMovement()).movement();
+    private int playerX, playerY;  // Position of the player.
+
+    public PlayerMovement(int startX, int startY) {
+        this.playerX = startX;
+        this.playerY = startY;
+    }
+
+    public int getPlayerX() {
+        // Mainly used for testing.
+        return playerX;
+    }
+
+    public int getPlayerY() {
+        // Mainly used for testing.
+        return playerY;
+    }
+
+    // Methods used to check the player movement (mainly for testing).
+    public void moveUp() {
+        playerY -= 1;
+    }
+
+    public void moveDown() {
+        playerY += 1;
+    }
+
+    public void moveLeft() {
+        playerX -= 1;
+    }
+
+    public void moveRight() {
+        playerX += 1;
+    }
+
+    @Override
+    public void keyPressed(KeyEvent e) {
+        int key = e.getKeyCode();
+
+        // The key will move the player based on 'WASD' or arrow keys.
+        if (key == KeyEvent.VK_W || key == KeyEvent.VK_UP) {
+            moveUp();
+        } else if (key == KeyEvent.VK_A || key == KeyEvent.VK_LEFT) {
+            moveLeft();
+        } else if (key == KeyEvent.VK_S || key == KeyEvent.VK_DOWN) {
+            moveDown();
+        } else if (key == KeyEvent.VK_D || key == KeyEvent.VK_RIGHT) {
+            moveRight();
+        }
+    }
+
+    @Override
+    public void keyTyped(KeyEvent e) {
+        // Used if necessary.
+    }
+
+    @Override
+    public void keyReleased(KeyEvent e) {
+        // Used if necessary.
     }
 }
-
