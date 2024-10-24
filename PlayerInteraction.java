f6da409fe365a3a532b8e26f5f68f40aab31726a
lmpham <long.m.phm@gmail.com>
2024-10-24 03:56:30 +0200

    Add inner class GameObject

--- PlayerInteraction.java
+++ PlayerInteraction.java
@@ -6,6 +6,27 @@ public class PlayerInteraction {
         this.player = player;
     }
 
+    // Inner class to represent a generic GameObject
+    public abstract class GameObject {
+        private int x, y;
+
+        public GameObject(int x, int y) {
+            this.x = x;
+            this.y = y;
+        }
+
+        public int getX() {
+            return x;
+        }
+
+        public int getY() {
+            return y;
+        }
+
+        public abstract void interact(PlayerMovement player);
+    }
+
+
     public boolean collidesWith(GameObject obj) {
         return player.getPlayerX() == obj.getX() && player.getPlayerY() == obj.getY(); 
     }
