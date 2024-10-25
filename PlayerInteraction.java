2acd448110e5634058c60d86bd3cd348fd38331e
lmpham <long.m.phm@gmail.com>
2024-10-25 03:24:49 +0200

    Removal of inner class GameObject

--- PlayerInteraction.java
+++ PlayerInteraction.java
@@ -5,28 +5,7 @@ public class PlayerInteraction {
     public PlayerInteraction(PlayerMovement player) {
         this.player = player;
     }
-
-    // Inner class to represent a generic GameObject
-    public abstract class GameObject {
-        private int x, y;
-
-        public GameObject(int x, int y) {
-            this.x = x;
-            this.y = y;
-        }
-
-        public int getX() {
-            return x;
-        }
-
-        public int getY() {
-            return y;
-        }
-
-        public abstract void interact(PlayerMovement player);
-    }
-
-
+    
     public boolean collisionChecker(GameObject obj) {
         return player.getPlayerX() == obj.getX() && player.getPlayerY() == obj.getY(); 
     }
