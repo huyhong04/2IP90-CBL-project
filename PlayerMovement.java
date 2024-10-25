25c95825a25ffa6e6799917237ec2e93e24ab43f
Huy Hong <kevinhong.nl@gmail.com>
2024-10-25 19:57:18 +0200

    Updated PlayerMovement file from tdd

--- PlayerMovement.java
+++ PlayerMovement.java
@@ -1,11 +1,104 @@
-public class PlayerMovement {
+import java.awt.event.*;
 
-    int movement() {
-        return 0;
-    }
+public class PlayerMovement implements KeyListener {
     
-    public static void main(String[] args) {
-        (new PlayerMovement()).movement();
+    private int playerX, playerY;  // Position of the player.
+    private int previousX, previousY; // Previous valid position.
+    private boolean dead;
+
+    public PlayerMovement(int startX, int startY) {
+        this.playerX = startX;
+        this.playerY = startY;
+        this.previousX = startX;
+        this.previousY = startY;
+        this.dead = false; // Player starts alive.
+
+    }
+
+    public int getPlayerX() {
+        // Mainly used for testing.
+        return playerX;
+    }
+
+    public int getPlayerY() {
+        // Mainly used for testing.
+        return playerY;
+    }
+
+    public int getPreviousX() {
+        return previousX;
+    }
+
+    public int getPreviousY() {
+        return previousY;
+    }
+
+    public void savePrevPosition() {
+        previousX = playerX; 
+        previousY = playerY;
+    }
+
+    // Revert to the previous position (stay still)
+    public void stayStill() {
+        playerX = previousX;
+        playerY = previousY;
+    }
+
+    public void die() {
+        dead = true;
+    }
+
+    public boolean isDead() {
+        return dead;
+    }
+
+    public void respawn(int newX, int newY) {
+        playerX = newX;
+        playerY = newY;
+        dead = false; 
+
+    }
+
+    // Methods used to check the player movement (mainly for testing).
+    public void moveUp() {
+        playerY -= 1;
+    }
+
+    public void moveDown() {
+        playerY += 1;
+    }
+
+    public void moveLeft() {
+        playerX -= 1;
+    }
+
+    public void moveRight() {
+        playerX += 1;
+    }
+
+    @Override
+    public void keyPressed(KeyEvent e) {
+        int key = e.getKeyCode();
+
+        // The key will move the player based on 'WASD' or arrow keys.
+        if (key == KeyEvent.VK_W || key == KeyEvent.VK_UP) {
+            moveUp();
+        } else if (key == KeyEvent.VK_A || key == KeyEvent.VK_LEFT) {
+            moveLeft();
+        } else if (key == KeyEvent.VK_S || key == KeyEvent.VK_DOWN) {
+            moveDown();
+        } else if (key == KeyEvent.VK_D || key == KeyEvent.VK_RIGHT) {
+            moveRight();
+        }
+    }
+
+    @Override
+    public void keyTyped(KeyEvent e) {
+        // Used if necessary.
+    }
+
+    @Override
+    public void keyReleased(KeyEvent e) {
+        // Used if necessary.
     }
 }
-
